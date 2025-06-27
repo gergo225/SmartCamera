c605ed9d2ce24cd766dd9772bb45b181f608f2e6
Gergo Fazekas <fazekasgergo225@gmail.com>
2025-06-27 18:26:58 +0300

    Initial Commit

---
+++ SmartCamera/SmartCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCameraApp.swift
+//  SmartCamera
+//
+//  Created by Fazekas, Gergo on 27.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
