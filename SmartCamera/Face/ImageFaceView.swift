8cbc7fd954a5c43b8543532e3fa3aa1f25da0b6a
Gergo Fazekas <fazekasgergo225@gmail.com>
2025-07-10 14:56:47 +0300

    Add toggles for various face features (jaw contour, eyes, mouth, nose) for face recognition

--- SmartCamera/Face/ImageFaceView.swift
+++ SmartCamera/Face/ImageFaceView.swift
@@ -13,6 +13,12 @@ struct ImageFaceView: View {
 
     @State private var selectedImage: PhotosPickerItem?
 
+    @State private var shouldShowFaceBox = true
+    @State private var shouldShowFaceContour = true
+    @State private var shouldShowEyes = true
+    @State private var shouldShowMouth = true
+    @State private var shouldShowNose = true
+
     var body: some View {
         VStack {
             if let photo = viewModel.photo {
@@ -21,24 +27,39 @@ struct ImageFaceView: View {
                     .scaledToFit()
                     .overlay {
                         ForEach(viewModel.faces, id: \.landmarkObservation.uuid) { face in
-                            let faceRect = face.normalizedRect
-                            BoundingBox(normalizedRect: faceRect)
-                                .stroke(.orange, lineWidth: 1)
+                            if shouldShowFaceBox {
+                                let faceRect = face.normalizedRect
+                                BoundingBox(normalizedRect: faceRect)
+                                    .stroke(.orange, lineWidth: 1)
+                            }
 
                             let landmarkObservation = face.landmarkObservation
-                            if let faceContour = landmarkObservation.landmarks?.faceContour {
-                                FaceLandmark(region: faceContour)
+
+                            if shouldShowFaceContour, let faceContour = landmarkObservation.landmarks?.faceContour {
+                                    FaceLandmark(region: faceContour)
+                                        .stroke(.blue, lineWidth: 2)
+                            }
+
+                            if shouldShowEyes {
+                                if let leftEye = landmarkObservation.landmarks?.leftEye {
+                                    FaceLandmark(region: leftEye)
+                                        .stroke(.green, lineWidth: 2)
+                                }
+
+                                if let rightEye = landmarkObservation.landmarks?.rightEye {
+                                    FaceLandmark(region: rightEye)
+                                        .stroke(.green, lineWidth: 2)
+                                }
+                            }
+
+                            if shouldShowMouth, let mouth = landmarkObservation.landmarks?.outerLips {
+                                FaceLandmark(region: mouth)
                                     .stroke(.blue, lineWidth: 2)
                             }
 
-                            if let leftEye = landmarkObservation.landmarks?.leftEye {
-                                FaceLandmark(region: leftEye)
-                                    .stroke(.green, lineWidth: 2)
-                            }
-
-                            if let rightEye = landmarkObservation.landmarks?.rightEye {
-                                FaceLandmark(region: rightEye)
-                                    .stroke(.green, lineWidth: 2)
+                            if shouldShowNose, let nose = landmarkObservation.landmarks?.nose {
+                                FaceLandmark(region: nose)
+                                    .stroke(.blue, lineWidth: 2)
                             }
                         }
                     }
@@ -53,6 +74,16 @@ struct ImageFaceView: View {
                 PhotosPicker(selection: $selectedImage, matching: .images) {
                     Text("Change Image")
                 }
+            }
+
+            Menu {
+                Toggle("Face Box", isOn: $shouldShowFaceBox)
+                Toggle("Face", isOn: $shouldShowFaceContour)
+                Toggle("Eyes", isOn: $shouldShowEyes)
+                Toggle("Mouth", isOn: $shouldShowMouth)
+                Toggle("Nose", isOn: $shouldShowNose)
+            } label: {
+                Image(systemName: "ellipsis")
             }
         }
         .onChange(of: selectedImage) { _, newImage in
